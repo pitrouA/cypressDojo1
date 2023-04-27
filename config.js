a78bc15185487e245be2dbfd7a2dbeaf2aa57014
pitrouA <adrien.pitrou@gmail.com>
2023-04-27 09:52:37 +0200

    1st commit to set all

---
+++ config.js
@@ -0,0 +1,5 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: '37170460-652b-4636-99d8-0b5ee78ded13',
+})
---
+++ cypress/e2e/nomdutest.cy.js
@@ -0,0 +1,7 @@
+describe("sgfsdgdfg", function(){
+  //TODO
+});
+
+describe('Titre de la suite de test', () => {
+    ....code de la suite de test
+})
